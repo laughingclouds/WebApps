23fce2f0cb9a62dc53f22a71a2d0f6ceccb0bb81
laughingclouds <zenithtaiga@gmail.com>
2021-08-11 13:10:26 +0530

    added the stuff for product-landing-page

--- projects/product-landing-page/src/app/app.module.ts
+++ projects/product-landing-page/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { FeatureSectionComponent } from './components/feature-section/feature-section.component';
+import { ProductFeatureComponent } from './components/feature-section/product-feature/product-feature.component';
+import { FooterNavbarComponent } from './components/footer-navbar/footer-navbar.component';
+import { HeaderNavbarComponent } from './components/header-navbar/header-navbar.component';
+import { PricingSectionComponent } from './components/pricing-section/pricing-section.component';
+import { PricingViewComponent } from './components/pricing-section/pricing-view/pricing-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    FeatureSectionComponent,
+    ProductFeatureComponent,
+    FooterNavbarComponent,
+    HeaderNavbarComponent,
+    PricingSectionComponent,
+    PricingViewComponent
   ],
   imports: [
     BrowserModule
---
+++ projects/product-landing-page/src/app/components/pricing-section/pricing-view/pricing-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing-view',
+  templateUrl: './pricing-view.component.html',
+  styleUrls: ['./pricing-view.component.scss']
+})
+export class PricingViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
