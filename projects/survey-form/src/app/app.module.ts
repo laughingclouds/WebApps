cfeb46080a6a2de08841a40c251dafe3ef099a6f
laughingclouds <zenithtaiga@gmail.com>
2021-08-12 11:53:14 +0530

    added angular material to one of the projects

--- projects/survey-form/src/app/app.module.ts
+++ projects/survey-form/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { FormComponent } from './components/form/form.component';
     FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
