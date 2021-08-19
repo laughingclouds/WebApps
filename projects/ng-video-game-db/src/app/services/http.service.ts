17a8abfaa29d8b53276647acc8aef97563997abc
laughingclouds <zenithtaiga@gmail.com>
2021-08-20 02:12:32 +0530

    noice

---
+++ projects/ng-video-game-db/src/app/services/http.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment as env } from '../../environments/environment';
+
+import { Observable } from 'rxjs';
+import { APIResponse, Game } from '../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  getGamesList(
+    ordering: string,
+    search?: string
+  ): Observable<APIResponse<Game>> {
+    let params = new HttpParams().set('ordering', ordering);
+
+    if (search) {
+      params = new HttpParams().set('ordering', ordering).set('search', search);
+    }
+
+    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
+      params: params,
+    });
+  }
+}
