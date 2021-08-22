8d577fed1d090635ddb02d124cc7f1a5b4ac2910
laughingclouds <zenithtaiga@gmail.com>
2021-08-23 00:23:13 +0530

    implemented onDestroy in home component

--- projects/ng-video-game-db/src/app/components/home/home.component.ts
+++ projects/ng-video-game-db/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { APIResponse, Game } from '../../models';
 import { HttpService } from '../../services/http.service';
 
@@ -8,18 +9,21 @@ import { HttpService } from '../../services/http.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  public sort?: string;
+  public sort!: string;
   public games?: Array<Game>
+  private routerSub!: Subscription;
+  private gameSub!: Subscription;
 
   constructor(
     private httpService: HttpService,
+    private router: Router,
     private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params: Params) => {
+    this.routerSub = this.activatedRoute.params.subscribe((params: Params) => {
       if (params['game-search']) {
         this.searchGames('metacritic', params['game-search']);
       } else {
@@ -29,7 +33,7 @@ export class HomeComponent implements OnInit {
   }
 
   searchGames(sort: string, search?: string): void {
-    this.httpService
+    this.gameSub = this.httpService
       .getGamesList(sort, search)
       .subscribe((gameList: APIResponse<Game>) => {
         this.games = gameList.results;
@@ -37,4 +41,17 @@ export class HomeComponent implements OnInit {
       });
   }
 
+  openGameDetails(id: string): void {
+    this.router.navigate(['details', id]);
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSub) {
+      this.routerSub.unsubscribe();
+    }
+
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+  }
 }
