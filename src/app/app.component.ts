f4448112e91cb57a9570f743798a87ee67d2a889
laughingclouds <zenithtaiga@gmail.com>
2021-08-30 15:34:59 +0530

    hmm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-projects';
+
+  clickSearchButtonNgVideoGameDB(): void {
+    /**
+     * This will click the class="search-button" of the id="ng-video-game-db" iframe.
+     * The api was not being invoked by default hence this is a workaround.
+    **/
+    ((document.getElementById("ng-video-game-db") as HTMLIFrameElement).contentWindow?.document.getElementsByClassName("search-button")[0] as HTMLButtonElement).click();
+  }
 }
